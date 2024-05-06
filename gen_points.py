447a51b3ecf81c9f48dfeda9610654230d80d81a
simone3c <154698047+simone3c@users.noreply.github.com>
2024-05-06 19:54:11 +0200

    fixed minimum length of segments

--- gen_points.py
+++ gen_points.py
@@ -1,17 +1,19 @@
 from random import uniform
-from math import sin, cos, pi
+from math import sin, cos, pi, tan
 
-n_points = 15
+n_points = 499
 radius = .5
+min_th = pi * .75
+min_edge_len = 2 * tan(min_th / 2)
 x = 0
 y = 0
 th = uniform(0, pi)
-with open(f"{n_points}_points.txt", "w") as f:
+with open(f"test/{n_points}_points.txt", "w") as f:
     f.write(f"{n_points}\n")
     while(n_points > 0):
         f.write(f"{x},{y}\n")
         n_points = n_points - 1
-        th = th + uniform(-pi * .75, pi * .75)
-        x = x + 4 * radius * cos(th)
-        y = y + 4 * radius * sin(th)
+        th = th + uniform(-min_th, min_th)
+        x = x + (min_edge_len + 1) * radius * cos(th)
+        y = y + (min_edge_len + 1) * radius * sin(th)
         
